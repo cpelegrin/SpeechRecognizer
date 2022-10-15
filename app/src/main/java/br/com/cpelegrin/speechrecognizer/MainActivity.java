00e4f3576efd6c89754d31c30d8e5a3747c046c7
Carlos Pelegrin <carloseduardopelegrin@gmail.com>
2022-10-15 17:41:08 -0300

    Alterações referentes a atualização do Android, como por exemplo atualizaçõesdo Gradle, Atualizações das novas features do Android X para start activity for result

--- app/src/main/java/br/com/cpelegrin/speechrecognizer/MainActivity.java
+++ app/src/main/java/br/com/cpelegrin/speechrecognizer/MainActivity.java
@@ -8,35 +8,94 @@ import android.os.Looper;
 import android.os.Message;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.List;
 import java.util.Locale;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private TextToSpeech ttsEngine;
-
     TextView speedValue;
     TextView pitchValue;
+    private TextToSpeech ttsEngine;
+    private final Handler handler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 0: {
+                    ttsEngine.speak("Oi Carlos", TextToSpeech.QUEUE_ADD, null, "UTTERANCE_ID");
+                    break;
+                }
+                case 1: {
+                    ttsEngine.speak("Tudo bem e você?", TextToSpeech.QUEUE_ADD, null, "UTTERANCE_ID");
+                    break;
+                }
+
+                case 2: {
+                    ttsEngine.speak("Que Bom! Boa noite alunos!", TextToSpeech.QUEUE_ADD, null, "UTTERANCE_ID");
+                    break;
+                }
+
+                case 3: {
+                    ttsEngine.speak("Esta é a aula 2.", TextToSpeech.QUEUE_ADD, null, "UTTERANCE_ID");
+                    break;
+                }
+
+                default: {
+                    ttsEngine.speak((CharSequence) msg.obj, TextToSpeech.QUEUE_ADD, null, "UTTERANCE_ID");
+                    break;
+                }
+            }
+        }
+    };
+    ActivityResultLauncher<Intent> activityResultLauncher =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    List<String> resultado =
+                            result.getData().getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String spokenText = resultado.get(0);
+
+                    Log.e(TAG, spokenText);
+
+                    spokenText = spokenText.toLowerCase();
+
+                    if (spokenText.contains("oi") || spokenText.contains("olá")) {
+                        handler.sendEmptyMessage(0);
+                    } else if (spokenText.contains("estou bem")) {
+                        handler.sendEmptyMessage(2);
+                    } else if (spokenText.contains(" bom") || spokenText.contains(" bem")) {
+                        handler.sendEmptyMessageDelayed(1, 500);
+                    } else if (spokenText.contains("aula")) {
+                        handler.sendEmptyMessage(3);
+                    } else {
+                        Message message = new Message();
+                        message.what = 800;
+                        message.obj = spokenText;
+                        handler.sendMessage(message);
+                    }
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -122,95 +181,10 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, new Locale("pt", "BR"));
 
         try {
-            startActivityForResult(intent, 1);
+//            startActivityForResult(intent, 1);
+            activityResultLauncher.launch(intent);
         } catch (ActivityNotFoundException a) {
             Log.e(TAG, "Your device does not support Speech to Text");
         }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == 1) {
-            if (resultCode == RESULT_OK && data != null) {
-                List<String> result =
-                        data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                String spokenText = result.get(0);
-
-                Log.e(TAG, spokenText);
-
-                spokenText = spokenText.toLowerCase();
-
-                if (spokenText.contains("oi") || spokenText.contains("olá") || spokenText.equals("oi")) {
-                    handler.sendEmptyMessage(0);
-                } else if (spokenText.contains("estou bem")) {
-                    handler.sendEmptyMessage(2);
-                } else if (spokenText.contains(" bom") || spokenText.contains(" bem")) {
-                    handler.sendEmptyMessageDelayed(1, 500);
-                } else if (spokenText.contains("aula")) {
-                    handler.sendEmptyMessage(3);
-                } else {
-                    Message message = new Message();
-                    message.what = 800;
-                    message.obj = spokenText;
-                    handler.sendMessage(message);
-                }
-            }
-        }
-    }
-
-    private Handler handler = new Handler(Looper.getMainLooper()) {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            switch (msg.what) {
-                case 0: {
-                    ttsEngine.speak("Oi Carlos", TextToSpeech.QUEUE_ADD, null, "UTTERANCE_ID");
-                    break;
-                }
-                case 1: {
-                    ttsEngine.speak("Tudo bem e você?", TextToSpeech.QUEUE_ADD, null, "UTTERANCE_ID");
-                    break;
-                }
-
-                case 2: {
-                    ttsEngine.speak("Que Bom! Boa noite alunos!", TextToSpeech.QUEUE_ADD, null, "UTTERANCE_ID");
-                    break;
-                }
-
-                case 3: {
-                    ttsEngine.speak("Esta é a aula 2.", TextToSpeech.QUEUE_ADD, null, "UTTERANCE_ID");
-                    break;
-                }
-
-                default: {
-                    ttsEngine.speak((CharSequence) msg.obj, TextToSpeech.QUEUE_ADD, null, "UTTERANCE_ID");
-                    break;
-                }
-            }
-        }
-    };
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
